63f5858afe29abeae2f9844b78f7459c4dde61e5
Skukar <101894352+Skukar@users.noreply.github.com>
2025-01-15 11:14:13 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -92,7 +92,7 @@ gem install lolcat
 apt install wondershaper -y
 clear
 # REPO    
-    REPO="https://raw.githubusercontent.com/BjhonStore/Bjhon/main/"
+    REPO="https://raw.githubusercontent.com/Skukar/gacor/main/"
 
 ####
 start=$(date +%s)
@@ -279,7 +279,7 @@ restart_system(){
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m" 
 clear
-izinsc="https://raw.githubusercontent.com/BjhonStore/Bjhon/main/register"
+izinsc="https://raw.githubusercontent.com/Skukar/gacor/main/register"
 # USERNAME
 rm -f /usr/bin/user
 username=$(curl $izinsc | grep $MYIP | awk '{print $2}')
